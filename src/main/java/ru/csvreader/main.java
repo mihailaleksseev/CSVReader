5445e28bacc9594dfbc204bb508d4e72141452c0
mihailalekssev <mihailaleksseev@yandex.ru>
2018-11-08 00:21:39 +0300

    Instruction for running in comment

--- src/main/java/ru/csvreader/main.java
+++ src/main/java/ru/csvreader/main.java
@@ -2,7 +2,8 @@ package ru.csvreader;
 
 public class main {
     public static void main(String[] args) throws Exception {
-//        new Preprocessing().Process(args[0], args[1], args[2]);
+//        new Preprocessing().Process(args[0], args[1], args[2])
+//        java -jar  CSVReader-1.0.0.jar tem.csv 1 2
         new PreprocessingCSV().Process(args[0], args[1], args[2]);
         System.out.println("start main");
     }
