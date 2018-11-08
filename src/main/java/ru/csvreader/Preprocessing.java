0ac5e2c910ad8a6f45172b5a2daf5a11f0b6a215
mihailaleksseev <mihailaleksseev@yandex.ru>
2018-11-08 19:09:03 +0300

    Removed openCSV

--- src/main/java/ru/csvreader/Preprocessing.java
+++ src/main/java/ru/csvreader/Preprocessing.java
@@ -1,51 +1,98 @@
 package ru.csvreader;
 
+import java.io.File;
 import java.io.PrintWriter;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 public class Preprocessing {
 
-    private String outputFile;
     private Boolean flagRemoveHeader = false;
 
-    public void Process(String inputFile, String numOutputFile, String removingColumnByNum) throws Exception {
-        System.out.println("start preprocc");
-        List<String> lines = Files.readAllLines(Paths.get("./../../files/" +
-                inputFile), Charset.forName("windows-1251"));
+    public void Process(String inputFile, String numOutputFile) throws Exception {
+        this.Process(inputFile, numOutputFile, null);
+    }
 
-        StringBuilder newLines = new StringBuilder();
+    public void Process(String inputFile, String numOutputFile, String removingColumn) throws Exception {
+        String readingFilePath = "./../../files/" + inputFile;
+        File readingFile = new File(readingFilePath);
 
-        System.out.println("removing column by num: " + removingColumnByNum);
+        if (readingFile.exists())
+        {
+            List<String> lines = Files.readAllLines(Paths.get(readingFilePath), Charset.forName("windows-1251"));
+            StringBuilder newLines = new StringBuilder();
 
-        for(String str : lines){
-            if (!flagRemoveHeader) {
-                flagRemoveHeader = true;
-                continue;
-            } else {
-                String newStr = "^" + str + "^";
-                newLines.append(newStr).append(System.lineSeparator());
-                String secondLine = newStr.substring(0,27)+ "2" +newStr.substring(28);
-                newLines.append(secondLine).append(System.lineSeparator());
+            for(String str : lines){
+                if (!flagRemoveHeader) {
+                    flagRemoveHeader = true;
+                    continue;
+                } else {
+
+                    System.out.println("removing column by num: " + removingColumn);
+                    this.removeColumns(str, removingColumn);
+
+                    String newStr = "^" + str + "^";
+
+                    newLines.append(newStr).append(System.lineSeparator());
+
+//                    String secondLine = newStr.substring(0,27)+ "2" +newStr.substring(28);
+                    newLines.append(newStr).append(System.lineSeparator());
+                }
             }
+
+            String text = newLines.toString();
+            text = text.replaceAll(";", "^");
+
+            DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+            Date date = new Date();
+
+            String outputFile = "VO_" + dateFormat.format(date) + "_" + numOutputFile + ".exb";
+
+            PrintWriter out = new PrintWriter("./../../files/" + outputFile);
+            out.println(text);
+            out.close();
+        } else {
+            System.out.println("File " + inputFile + " now found");
+        }
+    }
+
+
+    public String removeColumns(String startString, String removingColumn) {
+
+        ArrayList<String> lineWithoutSomeColumns = new ArrayList(Arrays.asList(startString.split(";")));
+
+        ArrayList<String> removingColumnsArray = new ArrayList(Arrays.asList(removingColumn.split(",")));
+        ArrayList<Integer> columns = getIntegerArray(removingColumnsArray);
+
+        lineWithoutSomeColumns.set(6, "2");
+        lineWithoutSomeColumns.remove(0);
+        lineWithoutSomeColumns.remove(1);
+        lineWithoutSomeColumns.remove(2);
+        for (int column : columns){
+            System.out.println("column " + column);
+            lineWithoutSomeColumns.remove(column);
         }
 
-        String text = newLines.toString();
-        text = text.replaceAll(";", "^");
+        String result = lineWithoutSomeColumns.toString();
+        System.out.println("result " + result);
 
-        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        Date date = new Date();
+        return result;
+    }
 
-        outputFile = "VO_" + dateFormat.format(date) + "_" + numOutputFile + ".exb";
-
-        PrintWriter out = new PrintWriter("./../../files/" + outputFile);
-        out.println(text);
-        out.close();
-
+    private ArrayList<Integer> getIntegerArray(ArrayList<String> stringArray) {
+        ArrayList<Integer> removingColumnIndexes = new ArrayList<Integer>();
+        for(String stringValue : stringArray) {
+            try {
+                removingColumnIndexes.add(Integer.parseInt(stringValue));
+            } catch(NumberFormatException nfe) {
+                System.out.println("Could not remove column with index " + stringValue);
+            }
+        }
+        Collections.sort(removingColumnIndexes, Collections.reverseOrder());
+        return removingColumnIndexes;
     }
 }
--- src/main/java/ru/csvreader/main.java
+++ src/main/java/ru/csvreader/main.java
@@ -2,9 +2,21 @@ package ru.csvreader;
 
 public class main {
     public static void main(String[] args) throws Exception {
-//        new Preprocessing().Process(args[0], args[1], args[2])
-//        java -jar  CSVReader-1.0.0.jar tem.csv 1 2
-        new PreprocessingCSV().Process(args[0], args[1], args[2]);
-        System.out.println("start main");
+        switch (args.length) {
+            case 3:
+                new Preprocessing().Process(args[0], args[1], args[2]);
+                System.out.println("3");
+                break;
+            case 2:
+                new Preprocessing().Process(args[0], args[1]);
+                System.out.println("2");
+                break;
+            case 1:
+            case 0:
+                System.out.println("Missed input or output file name");
+                break;
+        }
+
+
     }
 }
